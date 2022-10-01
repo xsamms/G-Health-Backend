b5cb330742cd647dc696ddf0f1b94ce93dc3e660
Samuel Williams <xsamms@outlook.com>
2022-10-01 01:38:35 +0100

    corrected product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -154,35 +154,35 @@ router.get(`/get/featured/:count`, async (req, res) => {
   res.send(products);
 });
 
-router.put(
-  "/gallery-images/:id",
-  uploadOptions.array("images", 4),
-  async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-      return res.status(400).send("Invalid Product Id");
-    }
-    const files = req.files;
-    let imagesPaths = [];
-    const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+// router.put(
+//   "/gallery-images/:id",
+//   uploadOptions.array("images", 4),
+//   async (req, res) => {
+//     if (!mongoose.isValidObjectId(req.params.id)) {
+//       return res.status(400).send("Invalid Product Id");
+//     }
+//     const files = req.files;
+//     let imagesPaths = [];
+//     const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
 
-    if (files) {
-      files.map((file) => {
-        imagesPaths.push(`${basePath}${file.filename}`);
-      });
-    }
+//     if (files) {
+//       files.map((file) => {
+//         imagesPaths.push(`${basePath}${file.filename}`);
+//       });
+//     }
 
-    const product = await Product.findByIdAndUpdate(
-      req.params.id,
-      {
-        images: imagesPaths,
-      },
-      { new: true }
-    );
+//     const product = await Product.findByIdAndUpdate(
+//       req.params.id,
+//       {
+//         images: imagesPaths,
+//       },
+//       { new: true }
+//     );
 
-    if (!product) return res.status(500).send("the gallery cannot be updated!");
+//     if (!product) return res.status(500).send("the gallery cannot be updated!");
 
-    res.send(product);
-  }
-);
+//     res.send(product);
+//   }
+// );
 
 module.exports = router;
